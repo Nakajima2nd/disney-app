bb356896b3eaa9c75ece4b0c013c8c96056266e7
Nakajima2nd <gp38ate@gmail.com>
2021-05-06 05:37:22 +0900

    material-uiとstyled-components用に設定を追加

---
+++ frontend/pages/_document.js
@@ -0,0 +1,47 @@
+import Document, { Html, Head, Main, NextScript } from 'next/document';
+import { ServerStyleSheet as StyledComponentSheets } from 'styled-components';
+import { ServerStyleSheets as MaterialUiServerStyleSheets } from '@material-ui/styles';
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html lang='ja'>
+        <Head />
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
+
+MyDocument.getInitialProps = async (ctx) => {
+  const styledComponentSheets = new StyledComponentSheets();
+  const materialUiServerStyleSheets = new MaterialUiServerStyleSheets();
+  const originalRenderPage = ctx.renderPage;
+
+  try {
+    ctx.renderPage = () =>
+      originalRenderPage({
+        enhanceApp: (App) => (props) =>
+          styledComponentSheets.collectStyles(materialUiServerStyleSheets.collect(<App {...props} />)),
+      });
+
+    const initialProps = await Document.getInitialProps(ctx);
+    return {
+      ...initialProps,
+      styles: (
+        <>
+          {initialProps.styles}
+          {styledComponentSheets.getStyleElement()}
+          {materialUiServerStyleSheets.getStyleElement()}
+        </>
+      ),
+    };
+  } finally {
+    styledComponentSheets.seal();
+  }
+};
