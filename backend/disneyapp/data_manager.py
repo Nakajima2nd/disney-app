9f56d8f992e8c2a3d5505aed92564d9f10b14fff
igarashi339 <igarashi339@gmail.com>
2021-06-05 06:47:44 +0000

    待ち時間などの動的情報をGASからfetchしてくる構成に変更

---
+++ backend/disneyapp/data_manager.py
@@ -0,0 +1,101 @@
+import json
+import time
+import urllib.request
+
+
+class DynamicDataManager:
+    """
+    待ち時間や運営中/停止中などの動的情報を管理するクラス。
+    """
+    GAS_URL = "https://script.google.com/macros/s/AKfycbzMWNM6QB2lgqFGcsyWHHvTinbNitmh2OmEPaXce8j8z6ufFf8mzojztc1nnj4nooF1jA/exec"
+    prev_fetch_time = 0
+    prev_fetch_data = {}
+
+    @classmethod
+    def fetch_latest_data(cls):
+        current_time = int(time.time())
+        if current_time - cls.prev_fetch_time < 600:
+            # 前回の取得から10分以内の場合は前回結果をそのまま返す
+            return cls.prev_fetch_data
+        cls.prev_fetch_time = current_time
+        url = DynamicDataManager.GAS_URL + "?mode=latest"
+        req = urllib.request.Request(url)
+        with urllib.request.urlopen(req) as res:
+            raw_dict = json.loads(res.read())
+            cls.prev_fetch_data = DynamicDataManager.parse_raw_data(raw_dict)
+        return cls.prev_fetch_data
+
+    @staticmethod
+    def parse_raw_data(raw_data_dict):
+        ret_dict = { "spots": {} }
+        for key in raw_data_dict:
+            if key == "timestamp":
+                ret_dict[key] = raw_data_dict[key]
+            else:
+                enable_str, wait_time = raw_data_dict[key].split(",")
+                enable = True if enable_str == "運営中" else False
+                ret_dict["spots"][key] = {
+                    "enable": enable,
+                    "wait-time": int(wait_time)
+                }
+        return ret_dict
+
+
+class StaticDataManager:
+    """
+    ネットワークデータやスポットデータなどの静的情報を管理するクラス。
+    """
+    __nodes = []
+    __links = []
+    __spots = []
+
+    @classmethod
+    def __load_nodes(cls):
+        with open("data/sea/nodes.json", "r", encoding="utf-8") as f:
+            json_data = json.load(f)
+            cls.__nodes = json_data["nodes"]
+
+    @classmethod
+    def __load_links(cls):
+        with open("data/sea/links.json", "r", encoding="utf-8") as f:
+            json_data = json.load(f)
+            cls.__links = json_data["links"]
+
+    @classmethod
+    def __load_spots(cls):
+        with open("data/sea/spots.json", "r", encoding="utf-8") as f:
+            json_data = json.load(f)
+            cls.__spots = json_data["spots"]
+
+    @classmethod
+    def get_nodes(cls):
+        if not cls.__nodes:
+            cls.__load_nodes()
+        return cls.__nodes
+
+    @classmethod
+    def get_links(cls):
+        if not cls.__links:
+            cls.__load_links()
+        return cls.__links
+
+    @classmethod
+    def get_spots(cls):
+        if not cls.__spots:
+            cls.__load_spots()
+        return cls.__spots
+
+
+def get_combined_spot_data():
+    """
+    Spot情報について、静的情報を動的情報を組み合わせた情報を返却する。
+    """
+    static_data = StaticDataManager.get_spots()
+    dynamic_data = DynamicDataManager.fetch_latest_data()
+    for elem in static_data:
+        name = elem["name"]
+        if not dynamic_data["spots"].get(name):
+            continue
+        elem["enable"] = dynamic_data["spots"][name]["enable"]
+        elem["wait-time"] = dynamic_data["spots"][name]["wait-time"]
+    return static_data
