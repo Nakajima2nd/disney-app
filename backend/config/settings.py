ac282bd57e67b3bcf941551486b9079336f69a06
Nakajima2nd <gp38ate@gmail.com>
2021-06-13 20:31:54 +0900

    REST API用の設定を追加

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -25,6 +25,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -35,7 +37,19 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.AllowAny'
+    ]
+}
+
+CORS_ORIGIN_WHITELIST = (
+    'http://127.0.0.1:3001',
+    'http://localhost:3001',
+)
 
 ROOT_URLCONF = 'config.urls'
 
