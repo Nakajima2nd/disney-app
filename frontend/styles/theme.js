4a1b653d5871dc5f73b9ca893ab54ed6e3b04af8
Nakajima2nd <gp38ate@gmail.com>
2021-06-13 20:39:40 +0900

    共通テーマを更新

--- frontend/styles/theme.js
+++ frontend/styles/theme.js
@@ -1,3 +1,23 @@
 import { createMuiTheme } from '@material-ui/core'
-const theme = createMuiTheme()
+import createBreakpoints from '@material-ui/core/styles/createBreakpoints'
+
+const breakpoints = createBreakpoints({})
+
+const theme = createMuiTheme({
+  overrides: {
+    MuiDialog: {
+      paperFullWidth: {
+        height: 'calc(100% - 64px)'
+      }
+    },
+    MuiTab: {
+      root: {
+        // minWidth: '0px',
+        [breakpoints.down("sm")]: {
+          // minWidth: '0px',
+        }
+      }
+    }
+  }
+})
 export default theme
