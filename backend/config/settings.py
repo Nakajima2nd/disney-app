d6a52faf56b1c454d099a41ef4fe9b9f26e754ce
igarashi339 <igarashi339@gmail.com>
2021-06-27 09:40:01 +0000

    setting.pyを修正

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -1,8 +1,8 @@
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
-
+# BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
@@ -14,7 +14,8 @@ SECRET_KEY = 'django-insecure-q8q=8!fvzj5up54!twyw1m6ia$ucjmyds&&y-(ti@r3#%vctd(
 DEBUG = True
 
 ALLOWED_HOSTS = [
-    "disney-app-api.herokuapp.com"
+    "disney-app-api.herokuapp.com",
+    "localhost"
 ]
 
 
