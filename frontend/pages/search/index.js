602693e476f075a0c11996e00f2734fe0e2823d9
Nakajima2nd <gp38ate@gmail.com>
2021-07-10 14:33:41 +0900

    不要なもの削除

--- frontend/pages/search/index.js
+++ frontend/pages/search/index.js
@@ -1,14 +1,11 @@
 import styled from 'styled-components'
 import { Box, Typography } from '@material-ui/core'
 import Link from 'next/link'
-import { searchSample } from '../../sample/search'
 
 const Text = styled(Typography)`
 `
-const data = searchSample
 
 const Search = () => {
-  console.log(data)
   return (<>
     <Link href="/">もどる</Link>
   </>)
