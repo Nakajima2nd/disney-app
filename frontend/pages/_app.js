0d3cb2e67ad454c64b405d0fa9e80a3a0c160a6c
Nakajima2nd <gp38ate@gmail.com>
2021-08-08 21:59:33 +0900

    共通スタイルを設定

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,21 +1,14 @@
 import '../styles/globals.css'
 import React, { useEffect } from 'react'
-import styled, { ThemeProvider as StyledComponentsThemeProvider } from 'styled-components'
+import { ThemeProvider as StyledComponentsThemeProvider } from 'styled-components'
 import {
   ThemeProvider as MaterialUIThemeProvider,
   StylesProvider
 } from '@material-ui/styles'
 import CssBaseline from '@material-ui/core/CssBaseline'
 import theme from '../styles/theme'
-import { Box } from '@material-ui/core'
 import DateFnsUtils from '@date-io/date-fns';
 import { MuiPickersUtilsProvider } from '@material-ui/pickers';
-
-const Wrap = styled(Box)`
-  margin: auto;
-  padding: 16px;
-  max-width: 800px;
-`
 
 const MyApp = ({ Component, pageProps }) => {
   // Remove the server-side injected CSS.(https://material-ui.com/guides/server-rendering/)
@@ -32,9 +25,7 @@ const MyApp = ({ Component, pageProps }) => {
         <StyledComponentsThemeProvider theme={theme}>
           <CssBaseline />
           <MuiPickersUtilsProvider utils={DateFnsUtils}>
-            <Wrap>
-              <Component {...pageProps} />
-            </Wrap>
+            <Component {...pageProps} />
           </MuiPickersUtilsProvider>
         </StyledComponentsThemeProvider>
       </MaterialUIThemeProvider>
